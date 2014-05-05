ca221b7df17d312550cbf7ed0ae99d8585142ae0
Michael Lancaster <michaellancaster@MacBook-Pro-de-Michael-Lancaster.local>
2014-05-05 14:46:04 -0500

    song list UI first version done

--- dev/app/public/js/controllers/stream.js
+++ dev/app/public/js/controllers/stream.js
@@ -3,7 +3,7 @@
 app.controller('StreamCtrl', ['$scope', '$http', function ($scope, $http) {
     var streamURL = 'https://api.soundcloud.com/me/activities?limit=10&oauth_token=' + window.scAccessToken;
 
-    $scope.title = 'Stream view';
+    $scope.title = 'Stream';
     $scope.data = '';
 
     $http({method: 'GET', url: streamURL})
