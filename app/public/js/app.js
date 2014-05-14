63587a418199a1f7ff901185f7131ef30526b94e
Michael Lancaster <michael.lancaster@leoburnett.com>
2014-05-14 17:09:32 -0500

    improve and fix gruntfile to build and compile SASS

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -10,32 +10,32 @@ app.config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
         .state('stream', {
             url: '/',
-            templateUrl: '../app/views/stream/stream.html',
+            templateUrl: 'views/stream/stream.html',
             controller: 'StreamCtrl'
         })
         .state('favorites', {
             url: '/favorites',
-            templateUrl: '../app/views/favorites/favorites.html',
+            templateUrl: 'views/favorites/favorites.html',
             controller: 'FavoritesCtrl'
         })
         .state('tracks', {
             url: '/tracks',
-            templateUrl: '../app/views/tracks/tracks.html',
+            templateUrl: 'views/tracks/tracks.html',
             controller: 'TracksCtrl'
         })
         .state('playlists', {
             url: '/playlists',
-            templateUrl: '../app/views/playlists/playlists.html',
+            templateUrl: 'views/playlists/playlists.html',
             controller: 'PlaylistsCtrl'
         })
         .state('following', {
             url: '/following',
-            templateUrl: '../app/views/following/following.html',
+            templateUrl: 'views/following/following.html',
             controller: 'FollowingCtrl'
         })
         .state('search', {
             url: '/search?q',
-            templateUrl: '../app/views/search/search.html',
+            templateUrl: 'views/search/search.html',
             controller: 'searchCtrl'
         });
 });
