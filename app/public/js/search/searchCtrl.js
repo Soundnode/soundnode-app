638341a0f3aea44891060afcb0d022a4de6f28cb
rodrigomaia17 <rodrigo.maia.pereira@gmail.com>
2014-07-28 02:42:52 -0300

    creating spinner css and adding it to the search template

--- app/public/js/search/searchCtrl.js
+++ app/public/js/search/searchCtrl.js
@@ -7,12 +7,15 @@ app.controller('searchCtrl', function ($scope, $http, $stateParams, SCapiService
     $scope.title = 'Results for: ' + $stateParams.q;
     $scope.data = '';
     $scope.busy = false;
+    $scope.isSearching = true;
 
     SCapiService.get(endpoint, params)
         .then(function(data) {
             $scope.data = data;
         }, function(error) {
             console.log('error', error);
+        }).finally(function(){
+            $scope.isSearching = false;
         });
 
 });
