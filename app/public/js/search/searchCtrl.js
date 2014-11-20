6ad21052d6eb795416fb5c43edf7873b20315586
joellundell <joel.lundell@gmail.com>
2014-11-20 17:04:13 +0100

    Fixed issue 61 - doesn't load more search results - created a wrapped api call in SCApiService and loads more search results using the SCapiService.newGetNextPage method

--- app/public/js/search/searchCtrl.js
+++ app/public/js/search/searchCtrl.js
@@ -1,19 +1,39 @@
 'use strict';
 
 app.controller('searchCtrl', function ($scope, $http, $stateParams, SCapiService, $rootScope) {
-    var endpoint = 'tracks'
-        , params = 'limit=51&q=' + $stateParams.q;
 
     $scope.title = 'Results for: ' + $stateParams.q;
     $scope.data = '';
+    var next_url = '';
+    var limit = 51;
 
-    SCapiService.get(endpoint, params)
+    SCapiService.searchTracks(limit, $stateParams.q)
         .then(function(data) {
-            $scope.data = data;
+            $scope.data = data.collection;
+            next_url = data.next_href;
         }, function(error) {
             console.log('error', error);
         }).finally(function(){
             $rootScope.isLoading = false;
         });
+
+    $scope.loadMore = function() {
+        if ($scope.busy) {
+            return;
+        }
+        $scope.busy = true;
+        SCapiService.newGetNextPage(next_url)
+            .then(function(data) {
+                for ( var i = 0; i < data.collection.length; i++ ) {
+                    $scope.data.push( data.collection[i] )
+                }
+                next_url = data.next_href;
+            }, function(error) {
+                console.log('error', error);
+            }).finally(function() {
+                $scope.busy = false;
+                $rootScope.isLoading = false;
+            });
+    };
 
 });
