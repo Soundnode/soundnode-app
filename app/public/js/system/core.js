9c2006ea8a77bf00aabf8203ce637bd589a7c7d7
Jon Kristensen <info@jonkri.com>
2014-12-16 03:54:51 +0100

    core.js: Make minimal changes to conform to the JSLint standard
    See #165.

--- app/public/js/system/core.js
+++ app/public/js/system/core.js
@@ -1,9 +1,14 @@
-var gui = require('nw.gui')
-    , uiFrame = {}
-    , OAuthVerification = {}
-    , OAuthVerificationId
-    , appGUI = {}
-    , appSystem = {};
+"use strict";
+
+// Avoid JSLint warning: "'$' was used before it was defined."
+/*global $, angular*/
+
+var gui = require('nw.gui'),
+    uiFrame = {},
+    OAuthVerification = {},
+    OAuthVerificationId,
+    appGUI = {},
+    appSystem = {};
 
 // Iframe hosting the OAuth
 var elemIframe = document.getElementById('elIframe');
@@ -16,78 +21,78 @@ var elemIframe = document.getElementById('elIframe');
 appGUI.getGUI = gui.Window.get();
 
 // close the App
-appGUI.close = function() {
-  var guiWin = this.getGUI;
-  guiWin.close(true);
-}
+appGUI.close = function () {
+    var guiWin = this.getGUI;
+    guiWin.close(true);
+};
 
 // minimize the App
-appGUI.minimize = function() {
-  var guiWin = this.getGUI;
-  guiWin.minimize();
-}
+appGUI.minimize = function () {
+    var guiWin = this.getGUI;
+    guiWin.minimize();
+};
 
 // maximize the App
-appGUI.maximize = function() {
-  var guiWin = this.getGUI;
-  guiWin.maximize();
-}
+appGUI.maximize = function () {
+    var guiWin = this.getGUI;
+    guiWin.maximize();
+};
 
 // open dev tools
-appGUI.openDevTools = function() {
+appGUI.openDevTools = function () {
     var guiWin = gui.Window.get();
     guiWin.showDevTools();
-}
+};
 
 //appGUI.openDevTools();
 
 /**
  * Responsible to enable all UI frame actions
  */
-uiFrame.init = function() {
+uiFrame.init = function () {
     // this.OpenDevTools();
     this.addGUIeventHandlers();
-}
+};
 
-uiFrame.addGUIeventHandlers = function() {
+uiFrame.addGUIeventHandlers = function () {
     // Get the current window
-    var elCloseApp = document.getElementById('closeApp')
-        , elMinimizeApp = document.getElementById('minimizeApp')
-        , elExpandApp = document.getElementById('expandApp');
+    var elCloseApp = document.getElementById('closeApp'),
+        elMinimizeApp = document.getElementById('minimizeApp'),
+        elExpandApp = document.getElementById('expandApp');
 
     // Close App
-    $(elCloseApp).on('click', function() {
+    $(elCloseApp).on('click', function () {
         appGUI.close();
     });
 
     // Minimize App
-    $(elMinimizeApp).on('click', function() {
+    $(elMinimizeApp).on('click', function () {
         appGUI.minimize();
     });
 
     // Expand App
-    $(elExpandApp).on('click', function() {
+    $(elExpandApp).on('click', function () {
         appGUI.maximize();
     });
 
-}
+};
 
 /**
  * Responsible to verify if user was verified
  */
-OAuthVerification.init = function() {
-    var that = this;
-    var popUp = window.open('http://sc-redirect.herokuapp.com/', '_blank', 'screenX=0,screenY=0,width=100,height=100');
+OAuthVerification.init = function () {
+    var that = this,
+        popUp = window.open('http://sc-redirect.herokuapp.com/', '_blank', 'screenX=0,screenY=0,width=100,height=100');
 
-    OAuthVerificationId = window.setInterval(function() {
+    OAuthVerificationId = window.setInterval(function () {
         that.verification(popUp);
     }, 1500);
-}
+};
 
-OAuthVerification.verification = function(popUp) {
+OAuthVerification.verification = function (popUp) {
     var isOAuthDone;
 
-    if ( popUp.document.body !== null ) {
+    if (popUp.document.body !== null) {
         isOAuthDone = popUp.document.body.getAttribute('data-isOAuth-done');
     } else {
         return;
@@ -112,15 +117,15 @@ OAuthVerification.verification = function(popUp) {
 
         console.log('verification done');
     }
-}
+};
 
 /**
  * Responsible to system events
  */
-appSystem.saveSong = function(songTitle, songUrl, folderPath) {
-    var url = songUrl + '?client_id=' + window.scClientId + + '&oauth_token=' + window.scAccessToken
-        , path = '.' + folderPath + '/' + songTitle;
-}
+// appSystem.saveSong = function (songTitle, songUrl, folderPath) {
+//     var url = songUrl + '?client_id=' + window.scClientId + '&oauth_token=' + window.scAccessToken,
+//         path = '.' + folderPath + '/' + songTitle;
+// };
 
 // Initialize all
 uiFrame.init();
