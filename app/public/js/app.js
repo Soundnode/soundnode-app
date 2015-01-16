3335a8028cdfecb2a6ef73fa1f55be356eeed49a
Michael Lancaster <mlancaster@mlancaster-mbp.duncllc.com>
2015-01-16 16:12:39 -0600

    closes #177 code refactoring and bugs fixes

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,4 +1,4 @@
-﻿'use strict'
+﻿'use strict';
 
 var app = angular.module('App', ['ui.router', 'ngSanitize', 'cfp.hotkeys', 'infinite-scroll', 'ngDialog']);
 
--- app/public/js/common/appCtrl.js
+++ app/public/js/common/appCtrl.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 app.controller('AppCtrl', function ($rootScope, $scope, $window, $log, ngDialog, hotkeys) {
 
