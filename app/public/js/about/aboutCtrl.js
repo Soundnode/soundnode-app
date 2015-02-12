208cb85a0b34f2c27fb34addbaa1fcd39a107f1c
Michael Lancaster <mlancaster@mlancaster-mbp.duncllc.com>
2015-02-12 12:46:20 -0600

    improve about view display current App version and latest App version. closes #204

--- app/public/js/about/aboutCtrl.js
+++ app/public/js/about/aboutCtrl.js
@@ -1,12 +1,19 @@
 'use strict';
 
-app.controller('AboutCtrl', function ($scope, $http, $rootScope, ngDialog) {
-    var url = 'https://api.github.com/repos/Soundnode/soundnode-about/contents/about.html'
+app.controller('AboutCtrl', function ($scope, $http, $rootScope, ngDialog, $window) {
+    var urlAbout = 'https://api.github.com/repos/Soundnode/soundnode-about/contents/about.html'
+        , config = { headers:  {
+                'Accept': 'application/vnd.github.v3.raw+json'
+            }
+        },
+        urlRelease = 'https://api.github.com/repos/Soundnode/soundnode-app/releases'
         , config = { headers:  {
                 'Accept': 'application/vnd.github.v3.raw+json'
             }
         };
 
+    $scope.appVersion = $window.settings.appVersion;
+    $scope.appLatestVersion = '';
     $scope.content = '';
 
     $scope.openModal = function() {
@@ -17,11 +24,28 @@ app.controller('AboutCtrl', function ($scope, $http, $rootScope, ngDialog) {
         });
     };
 
-    $http.get(url, config)
+    /**
+     * Get Soundnode about.html form Github
+     */
+    $http.get(urlAbout, config)
         .success(function (data) {
             $scope.content = data;
         })
         .error(function (error) {
             console.log('Error', error)
         });
+
+
+    /**
+     * Get App version from latest release from Github
+     */
+    $http.get(urlRelease, config)
+        .success(function (data) {
+            var release = data[0];
+            $scope.appLatestVersion = release.tag_name;
+        })
+        .error(function (error) {
+            console.log('Error on getting latest release', error);
+        });
+
 });
--- app/public/js/updater/updaterCtrl.js
+++ app/public/js/updater/updaterCtrl.js
@@ -1,9 +1,9 @@
 app.controller('UpdaterCtrl', function($scope, $http, $window) {
     var url = 'https://api.github.com/repos/Soundnode/soundnode-app/releases'
         , config = { headers:  {
-                'Accept': 'application/vnd.github.v3.raw+json'
-            }
-        };
+                    'Accept': 'application/vnd.github.v3.raw+json'
+                }
+            };
 
     $scope.updateAvailable = false;
 
