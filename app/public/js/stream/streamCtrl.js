bfb3ed998ab83ea7cb6a3c2950a316342daed38c
Pitros <xpitrosx@gmail.com>
2015-09-08 21:38:12 +0200

    Mark tracks as liked on stream view

--- app/public/js/stream/streamCtrl.js
+++ app/public/js/stream/streamCtrl.js
@@ -7,15 +7,25 @@ app.controller('StreamCtrl', function ($scope, SCapiService, $rootScope) {
     $scope.title = 'Stream';
     $scope.data = '';
     $scope.busy = false;
+    $scope.likes = '';
 
     SCapiService.get(endpoint, params)
+        .then(function(data) {
+            $scope.data = data.collection;
+        }, function(error) {
+            console.log('error', error);
+        }).finally(function() {
+
+            SCapiService.getFavoritesIds()
                 .then(function(data) {
-                    $scope.data = data.collection;
+                    $scope.likes = data;
+                    markLikedTracks($scope.data);
                 }, function(error) {
                     console.log('error', error);
                 }).finally(function() {
                     $rootScope.isLoading = false;
                 });
+        });
 
     $scope.loadMore = function() {
         if ( $scope.busy ) {
@@ -25,6 +35,7 @@ app.controller('StreamCtrl', function ($scope, SCapiService, $rootScope) {
 
         SCapiService.getNextPage()
             .then(function(data) {
+                markLikedTracks(data);
                 for ( var i = 0; i < data.collection.length; i++ ) {
                     $scope.data.push( data.collection[i] )
                 }
@@ -35,5 +46,15 @@ app.controller('StreamCtrl', function ($scope, SCapiService, $rootScope) {
                 $rootScope.isLoading = false;
             });
     };
+
+    function markLikedTracks (tracks) {
+        var tracksData = tracks.collection || tracks;
+        for (var i = 0; i < tracksData.length; ++i) {
+            var track = tracksData[i].origin;
+
+            if (track.hasOwnProperty('user_favorite'))
+                track.user_favorite = ($scope.likes.indexOf(track.id) != -1);
+        }
+    }
 
 });
