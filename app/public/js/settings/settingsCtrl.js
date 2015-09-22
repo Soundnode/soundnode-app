29efaa1724526f52055e59f7295de38a6704c8b8
MLancaster <michaell.llancaster@gmail.com>
2015-09-22 14:37:54 -0500

    settings feat code refactoring

--- app/public/js/settings/settingsCtrl.js
+++ app/public/js/settings/settingsCtrl.js
@@ -1,19 +1,19 @@
 'use strict';
 
 app.controller('SettingsCtrl', function ($scope, $rootScope) {
+    $scope.title = "Settings";
+
+    if ( window.localStorage.notificationToggle ) {
+        $scope.notification = JSON.parse(window.localStorage.notificationToggle);
+    } else {
+        window.localStorage.notificationToggle = $scope.notification = true;
+    }
+
+    $scope.label = window.localStorage.notificationToggle ? "enabled": "disabled";
+
     $scope.notificationSettings = function() {
         window.localStorage.notificationToggle = $scope.notification;
+        $scope.label = $scope.notification ? "enabled": "disabled";
     };
 
-    $scope.init = function() {
-        if(window.localStorage.notificationToggle === undefined) {
-            window.localStorage.notificationToggle = true;
-            $scope.notification = true;
-        }
-        else {
-            $scope.notification = JSON.parse(window.localStorage.notificationToggle);
-        }
-    };
-
-    $scope.init();
 });
