07f1713d30c33afd6797ac3f7277147c338f3b44
Michael Lancaster <michaell.llancaster@gmail.com>
2015-11-02 11:29:10 -0600

    add webpack to bundle and build react/jsx. Grunt tasks improved to accomodate webpack. npm script added to accomodate webpack

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,7 +1,8 @@
 module.exports = {
     src: {
         files: [
-            '<%= settings.app %>/public/stylesheets/sass/**/*.scss'
+            '<%= settings.app %>/public/stylesheets/sass/**/*.scss',
+            '<%= settings.app %>/public/js/**/*.jsx'
         ],
         tasks: ['dev']
     }
