78fb06d138a672b5e61917ee8693bf1a3b1c0968
Michael Lancaster <michaell.llancaster@gmail.com>
2015-11-04 14:21:09 -0600

    remove use strict since ES2015 module pattern have as default

--- app/public/js/components/header/headerActions.jsx
+++ app/public/js/components/header/headerActions.jsx
@@ -1,5 +1,3 @@
-"use strict";
-
 import React, { Component } from 'react';
 
 class BackForwardActions extends Component {
--- app/public/js/components/main.jsx
+++ app/public/js/components/main.jsx
@@ -1,5 +1,3 @@
-"use strict";
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
