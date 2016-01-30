d039900f528e553f35f4ab3efbbadb6f6745e684
Ryan <fishr01@gmail.com>
2016-01-30 02:10:28 +0200

    Reworked Top 50 Charts to be able to filter by genre - much like on the current soundcloud website

--- app/public/js/charts/chartsCtrl.js
+++ app/public/js/charts/chartsCtrl.js
@@ -2,24 +2,163 @@
 
 app.controller('ChartsCtrl', function (
     $scope,
+    $filter,
     $rootScope,
     SCapiService,
     SC2apiService,
-    utilsService
+    utilsService,
+    $stateParams
 ) {
     var tracksIds = [];
+    $scope.genres = [
+        {
+            "link": 'all-music',
+            "title": "All"
+        },
+        {
+            "link": 'alternativerock',
+            "title": "Alternative Rock"
+        },
+        {
+            "link": 'ambient',
+            "title": "Ambient"
+        },
+        {
+            "link": 'classical',
+            "title": "Classical"
+        },
+        {
+            "link": 'country',
+            "title": "Country"
+        },
+        {
+            "link": 'danceedm',
+            "title": "Dance & EDM"
+        },
+        {
+            "link": 'dancehall',
+            "title": "Dancehall"
+        },
+        {
+            "link": 'deephouse',
+            "title": "Deep House"
+        },
+        {
+            "link": 'disco',
+            "title": "Disco"
+        },
+        {
+            "link": 'drumbass',
+            "title": "Drum & Bass"
+        },
+        {
+            "link": 'dubstep',
+            "title": "Dubstep"
+        },
+        {
+            "link": 'electronic',
+            "title": "Electronic"
+        },
+        {
+            "link": 'folksingersongwriter',
+            "title": "Folk & Singer-Songwriter"
+        },
+        {
+            "link": 'hiphoprap',
+            "title": "Hip-hop & Rap"
+        },
+        {
+            "link": 'house',
+            "title": "House"
+        },
+        {
+            "link": 'indie',
+            "title": "Indie"
+        },
+        {
+            "link": 'jazzblues',
+            "title": "Jazz & Blues"
+        },
+        {
+            "link": 'latin',
+            "title": "Latin"
+        },
+        {
+            "link": 'metal',
+            "title": "Metal"
+        },
+        {
+            "link": 'piano',
+            "title": "Piano"
+        },
+        {
+            "link": 'pop',
+            "title": "Pop"
+        },
+        {
+            "link": 'rbsoul',
+            "title": "R&B & Soul"
+        },
+        {
+            "link": 'reggae',
+            "title": "Reggae"
+        },
+        {
+            "link": 'reggaeton',
+            "title": "Reggaeton"
+        },
+        {
+            "link": 'rock',
+            "title": "Rock"
+        },
+        {
+            "link": 'soundtrack',
+            "title": "Soundtrack"
+        },
+        {
+            "link": 'techno',
+            "title": "Techno"
+        },
+        {
+            "link": 'trance',
+            "title": "Trance"
+        },
+        {
+            "link": 'trap',
+            "title": "Trap"
+        },
+        {
+            "link": 'triphop',
+            "title": "Triphop"
+        },
+        {
+            "link": 'world',
+            "title": "World"
+        }
+    ]
 
-    $scope.title = 'Top 50';
+    var url_genre = $stateParams.genre;
+    var genre = {};
+    if(!url_genre){
+        genre = {
+            "link" : "all-music",
+            "title" : "All Music"
+        }
+    }else{
+        genre = $filter('filter')($scope.genres, {"link":url_genre}, true)[0]
+    }
+
+    $scope.title = 'Top 50 - '+genre.title;
     $scope.originalData = '';
     $scope.data = '';
     $scope.busy = false;
 
-    SC2apiService.getCharts()
+
+    SC2apiService.getCharts(genre.link)
         .then(filterCollection)
         .then(function (collection) {
             $scope.originalData = collection;
             $scope.data = collection;
-
             loadTracksInfo(collection);
         })
         .catch(function (error) {
