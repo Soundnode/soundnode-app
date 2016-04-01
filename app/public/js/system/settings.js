eb71c0488af44d818373d142c23ae26eca035fd2
Michael Lancaster <michaell.llancaster@gmail.com>
2016-03-31 23:47:42 -0500

    bump version

--- app/public/js/system/settings.js
+++ app/public/js/system/settings.js
@@ -6,7 +6,7 @@ var ua = require('universal-analytics');
 window.settings = {};
 
 // App version
-window.settings.appVersion = '0.6.3';
+window.settings.appVersion = '0.6.4';
 
 // GA >> DO NOT CHANGE OR USE THIS CODE <<
 window.settings.visitor = ua('UA-67310953-1');
