66a2ddfcd0fece618e9cdd87679480870565248b
Jake Jarrett <jakeryanjarrett@gmail.com>
2016-06-13 13:56:34 +1000

    mpris support
    Added some abstracted functionality so we don't define 4 of the same if statements
    Added proper stop functionality (Set time to 0 & Pause)
    Moved mpris keypress events into playerCtrl instead of playerService.
    Fixed grunt shell for non-linux os'
    
    This should be much easier to follow whats going on.

--- tasks/shell.js
+++ tasks/shell.js
@@ -3,6 +3,12 @@ module.exports = {
         stdout: true
     },
     target: {
-        command: 'webpack -p'
+        command: function() {
+            if("linux" === process.platform) {
+                return 'webpack -p && export PYTHON=/usr/bin/python2 && cd ./node_modules/mpris-service/node_modules/dbus && nw-gyp rebuild --target=0.12.3';
+            } else {
+                return 'webpack -p';
+            }
+        }
     }
 };
