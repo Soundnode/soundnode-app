72584f9806de595ffaf69fb99d3607d369d219c3
Jonas Gröger <jonas@huntun.de>
2016-07-24 14:32:25 +0200

    Make build work without elevated provileges
    First, we need `make build` install grunt-cli and
    webpack locally (under node_modules/).
    
    Second, `grunt`, `nw-gyp`  and `webpack` are no longer globally available:
        $ webpack
        $ grunt
        $ nw-gyp
    but rather deep in node_modules:
        $ node_modules/webpack/bin/webpack.js
        $ node_modules/grunt-cli/bin/grunt
        $ node_modules/nw-gyp/bin/nw-gyp.js

--- Makefile
+++ Makefile
@@ -4,14 +4,19 @@ GIT_VERSION := $(shell git describe --tags --long --dirty --always)
 
 .PHONY: build
 build: ## Build the project
-	sudo npm install --global grunt-cli webpack
+	npm install grunt-cli webpack nw-gyp request
 	npm install --legacy-bundling
-	grunt build
+	node_modules/grunt-cli/bin/grunt build
 
 .PHONY: deb
 deb: build ## Build debian packages (32bit + 64bit)
 	VERSION=$(GIT_VERSION) DIST=linux32 ./fpm/build-deb
 	VERSION=$(GIT_VERSION) DIST=linux64 ./fpm/build-deb
+
+clean:
+	git clean -fdX
+	rm -f fpm/soundnode-48x48.png
+	rm -rf cache/0.12.3/osx64/nwjs-v0.12.3-osx-x64/
 
 .PHONY: help
 help:
--- tasks/shell.js
+++ tasks/shell.js
@@ -4,10 +4,14 @@ module.exports = {
     },
     target: {
         command: function() {
+            var webpack = 'node_modules/webpack/bin/webpack.js -p';
+            var nw_gyp = 'node_modules/nw-gyp/bin/nw-gyp.js rebuild --directory=node_modules/mpris-service/node_modules/dbus --target=0.12.3';
+            var export_py2 = 'export PYTHON=/usr/bin/python2';
+
             if("linux" === process.platform) {
-                return 'webpack -p && export PYTHON=/usr/bin/python2 && cd ./node_modules/mpris-service/node_modules/dbus && nw-gyp rebuild --target=0.12.3';
+                return [webpack, export_py2, nw_gyp].join(' && ');
             } else {
-                return 'webpack -p';
+                return webpack;
             }
         }
     }
