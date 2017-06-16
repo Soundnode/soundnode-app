c4e3681cacb0f0d79b958577b3e93df72bcf8b09
Jake Jarrett <jakeryanjarrett@gmail.com>
2017-06-17 09:44:10 +1000

    Ensure the folder exists before trying to modify its children
    This adds a private function (Possibly could abstract it out to a class,
    but it's not used anywhere else) to simply determine if a given location
    is a folder & if it exists.

--- app/public/js/common/configLocation.js
+++ app/public/js/common/configLocation.js
@@ -1,33 +1,58 @@
 const fs = require('fs-extra');
 const userHome = require('user-home');
+const mkdirp = require('mkdirp');
+
+/**
+ * Check if a given folder exists before trying to access any of it's children
+ * 
+ * @param {String} folder The folder we're checking
+ */
+const folderExists = folder => {
+    let exists = false;
+    fs.stat(folder, (err, stats) => { if (stats.isDirectory()) exists = true; });
+    return exists;
+};
 
 class Configuration {
 
     /**
-     * Get the configuration path
+     * Get the configuration folder location
      * 
-     * @returns {string} The location of the config
+     * @returns {string} The folder location of the config
      */
-    static get path () {
+    static get location () {
         let userConfigPath = null;
 
         /** Linux platforms - XDG Standard */
         if (process.platform === 'win32') {
-            userConfigPath = `${userHome}/.config/Soundnode/userConfig.json`;
+            userConfigPath = `${userHome}/.config/Soundnode`;
         }
 
 
         /** Linux platforms - XDG Standard */
         if (process.platform === 'linux') {
-            userConfigPath = `${userHome}/.config/Soundnode/userConfig.json`;
+            userConfigPath = `${userHome}/.config/Soundnode`;
         }
 
         /** Mac os configuration location */
         if (process.platform === 'darwin') {
-            userConfigPath = `${userHome}/Library/Preferences/Soundnode/userConfig.json`;
+            userConfigPath = `${userHome}/Library/Preferences/Soundnode`;
+        }
+
+        if (!folderExists(userConfigPath)) {
+            mkdirp(userConfigPath, err => { if (err) console.error(err); });
         }
 
         return userConfigPath;
+    }
+
+    /**
+     * Get the configuration path
+     * 
+     * @returns {string} The file location of the config
+     */
+    static get path () {
+        return `${this.location}/userConfig.json`
     }
 
     /**
@@ -45,7 +70,7 @@ class Configuration {
      * @returns {Boolean} True if the file exists
      */
     static get configExists () {
-        return fs.existsSync(this.path);
+        return fs.existsSync(`${this.path}`);
     }
 
 }
