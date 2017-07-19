d908edc1a4d0d663b59d4ec607110a7b9aece059
Michael Lancaster <michaell.llancaster@gmail.com>
2017-07-19 14:38:10 -0700

    Move Configuration to a object and methods
    Instead of using class we will use a obj that contain the get methods needed that way making simpler when importing in other files.

--- app/public/js/common/configLocation.js
+++ app/public/js/common/configLocation.js
@@ -1,71 +1,79 @@
+'use strict';
+
 const fs = require('fs-extra');
 const userHome = require('user-home');
 const mkdirp = require('mkdirp');
 
-class Configuration {
+const configuration = {
 
-    /**
-     * Get the configuration folder location
-     * 
-     * @returns {string} The folder location of the config
-     */
-    static get location () {
-        let userConfigPath = null;
+  createUserConfig(userConfigPath) {
+    mkdirp(userConfigPath, err => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  },
 
-        /** Windows platform */
-        if (process.platform === 'win32') {
-            userConfigPath = `${userHome}/.config/Soundnode`;
-        }
+  /**
+   * Get the configuration folder location
+   *
+   * @returns {string} The folder location of the config
+   */
+  getUserConfig() {
+    let userConfigPath = null;
 
-
-        /** Linux platforms - XDG Standard */
-        if (process.platform === 'linux') {
-            userConfigPath = `${userHome}/.config/Soundnode`;
-        }
-
-        /** Mac os configuration location */
-        if (process.platform === 'darwin') {
-            userConfigPath = `${userHome}/Library/Preferences/Soundnode`;
-        }
-
-        if (!fs.statSync(userConfigPath).isDirectory()) {
-            mkdirp(userConfigPath, err => {
-                if (err) {
-                    console.error(err);
-                }
-            });
-        }
-
-        return userConfigPath;
+    /** Windows platform */
+    if (process.platform === 'win32') {
+      userConfigPath = `${userHome}/.config/Soundnode`;
     }
 
-    /**
-     * Get the configuration path
-     * 
-     * @returns {string} The file location of the config
-     */
-    static get path () {
-        return `${this.location}/userConfig.json`
+
+    /** Linux platforms - XDG Standard */
+    if (process.platform === 'linux') {
+      userConfigPath = `${userHome}/.config/Soundnode`;
     }
 
-    /**
-     * Get the config file
-     * 
-     * @returns {Object} Parsed version of the saved file
-     */
-    static get file () {
-        return JSON.parse(fs.readFileSync(`${this.path}`, 'utf-8'));
+    /** Mac os configuration location */
+    if (process.platform === 'darwin') {
+      userConfigPath = `${userHome}/Library/Preferences/Soundnode`;
     }
 
-    /**
-     * Ensure the config exists
-     * 
-     * @returns {Boolean} True if the file exists
-     */
-    static get configExists () {
-        return fs.existsSync(`${this.path}`);
+    // create user config in path
+    // if there is no userConfig path
+    if (!fs.statSync(userConfigPath).isDirectory()) {
+      this.createUserConfig()
     }
+
+    return userConfigPath;
+  },
+
+  /**
+   * Get the configuration path
+   *
+   * @returns {string} The file location of the config
+   */
+  getPath() {
+    return `${this.getUserConfig()}/userConfig.json`
+  },
+
+  /**
+   * Get the config file
+   *
+   * @returns {Object} Parsed version of the saved file
+   */
+  getConfigfile() {
+    return JSON.parse(fs.readFileSync(`${this.getPath()}`, 'utf-8'));
+  },
+
+  /**
+   * Ensure the config exists
+   *
+   * @returns {Boolean} True if the file exists
+   */
+  containsConfig() {
+    return fs.existsSync(`${this.getPath()}`);
+  }
 
 }
 
-module.exports = Configuration;
+module.exports = configuration;
